899ebd0f458e6c963693878a9abed83059492f21
narayan singh <drnarayanbamania@gmail.com>
2026-01-03 23:51:26 -0800

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,1 +1,20 @@
+function App() {
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        backgroundColor: "#020617",
+        color: "white",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "28px",
+        fontWeight: "bold",
+      }}
+    >
+      ✅ BAMANIA AI APP WORKING
+    </div>
+  );
+}
 
+export default App;
